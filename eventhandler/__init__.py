565b32c6e046f1dc2fdf444b8c52c82fd7847834
null <null>
2020-09-15 18:37:48 +0100

    refactoring

--- eventhandler/__init__.py
+++ eventhandler/__init__.py
@@ -1,83 +1,116 @@
 import sys
 import types
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 
 class EventHandler:
     """Event manager, bind, unbind and fire events."""
 
     class Exceptions:
+        """Custom error classes."""
         class EventNotAllowedError(Exception):
             pass
 
-        class InitializationError(Exception):
-            pass
+    events: dict = {}  # Wil hold events names as keys and callbacks as value list of the event key.
+    tolerate_exceptions = False  # Do not raise an error if a callback fails
 
-    class Callbacks:
-        """Empty class to hold callbacks callables."""
-        pass
-
-    events: dict = None
-    allowed_callbacks = []
-
-    def __init__(self, *args, verbose=False):
-        """EventHandler initiazition recibe a list of allowed event names."""
+    def __init__(self, *event_names, verbose=False, file_to_verbose=sys.stdout, tolerate_callbacks_exceptions=False):
+        """EventHandler initiazition recibes a list of allowed event names as arguments."""
         self.verbose = verbose
-        if not args:
-            raise EventHandler.Exceptions.InitializationError(
-                'EventHandler must be initialized with a list of callbacks')
+        self.tolerate_exceptions = tolerate_callbacks_exceptions
+        self.print_file = file_to_verbose
+        if event_names:
+            for event in event_names:
+                self.register_event(str(event)) # cast as str to be safe
 
-        self.set_allowed_callbacks(args) if args else None
+    @property
+    def registered_events(self) -> [str]:
+        """Retun  list of regitered events."""
+        return self.events.keys()
 
-    def set_allowed_callbacks(self, allowed_callbacks: [str]):
-        """Set the allowed events names"""
-        # remove previous
-        for callback in self.allowed_callbacks:
-            delattr(self.Callbacks, callback.upper())
+    def event_registered(self, event_name: str) -> bool:
+        """Return if an event is current registered."""
+        return event_name in self.events
 
-        self.allowed_callbacks = allowed_callbacks
+    def register_event(self, event_name: str) -> bool:
+        """Register an event name."""
+        # print('registering event', event_name, self.events)
+        if event_name in self.events:
+            print(f'Omiting. {event_name} is already implemented', file=self.print_file) if self.verbose else None
+            return False
 
-        for callback in self.allowed_callbacks:
-            setattr(self.Callbacks, callback.upper(), [])
+        self.events[event_name] = []
+        return True
+
+    def unregister_event(self, event_name: str) -> bool:
+        """Unregister an event name."""
+        if event_name in self.events:
+            del self.events[event_name]
+            return True
+        print(f'Omiting unregister_event. {event_name} '
+              f'is not implemented.', file=self.print_file) if self.verbose else None
+        return False
 
     def is_callable(self, func: callable) -> bool:
         """Return true if func is a callable variable."""
         return isinstance(func,
                           (types.FunctionType, types.BuiltinFunctionType, types.MethodType, types.BuiltinMethodType))
 
-    def bind(self, event: str, callback: callable) -> bool:
+    def bind(self, event_name: str, callback: callable) -> bool:
         """Bind a callback to an event."""
+
         if not self.is_callable(callback):
             print(f'Callback not registered. Type {type(callback)} '
-                  f'is not a callable function.', file=sys.stdout) if self.verbose else None
+                  f'is not a callable function.', file=self.print_file) if self.verbose else None
             return False
 
-        if not event in self.allowed_callbacks:
+        if not self.event_registered(event_name):
             raise EventHandler.Exceptions.EventNotAllowedError(
-                f'Event {event} is not allowed. Allowed events are: {self.allowed_callbacks}. '
-                f'Please register event {event} on object initialization.')
+                f'Can not bind event {event_name}, not registered. Registered events are:'
+                f' {", ".join(self.events.keys())}. Please register event {event_name} before bind callbacks.')
 
-        active_list = getattr(self.Callbacks, event.upper())
-        if callback not in active_list:
-            active_list.append(callback)
-            setattr(self.Callbacks, event.upper(), active_list)
+        if callback not in self.events[event_name]:
+            self.events[event_name].append(callback)
             return True
 
-        print(f'Event {str(callback.__name__)} is already registered in '
-              f'{event.upper()} event.', file=sys.stdout) if self.verbose else None
+        print(f'Can not bind callback {str(callback.__name__)}, already registered in '
+              f'{event_name} event.', file=self.print_file) if self.verbose else None
         return False
 
-    def fire(self, event: str, *args, **kwargs):
-        """Fire a callback to an event."""
-        active_list = getattr(self.Callbacks, event.upper(), None)
-        for callback in active_list:
-            callable(callback(*args, **kwargs))
+    def fire(self, event_name: str, *args, **kwargs) -> bool:
+        """Fire an event callbacks and return True only if all callbacks was successfully."""
 
-    def unbind(self, event: str, callback: callable) -> bool:
+        all_ok = True
+        for callback in self.events[event_name]:
+            try:
+                callable(callback(*args, **kwargs))
+            except Exception as e:
+                if not self.tolerate_exceptions:
+                    raise e
+                else:
+                    if self.verbose:
+                        print(f'WARNING: {str(callback.__name__)} produces an exception error.', file=self.print_file)
+                        print('Arguments', args, file=self.print_file)
+                        print(e, file=self.print_file)
+                        all_ok = False
+                    continue
+
+        return all_ok
+
+    def unbind(self, event_name: str, callback: callable) -> bool:
         """Unbind a callback from an event."""
-        active_list = getattr(self.Callbacks, event.upper())
-        if callback in active_list:
-            active_list.remove(callback)
+        if not self.event_registered(event_name):
+            print(f'Can not unbind event {event_name}, not registered. Registered events '
+                  f'are: {", ".join(self.events.keys())}. '
+                  f'Please register event {event_name} before unbind callbacks.', file=self.print_file)
+            return False
+
+        if callback in self.events[event_name]:
+            self.events[event_name].remove(callback)
             return True
+
+        print(f'Can not unbind callback {str(callback.__name__)}, is not registered in '
+              f'{event_name} event.', file=self.print_file) if self.verbose else None
+
         return False
--- tests/test_eventHandler.py
+++ tests/test_eventHandler.py
@@ -2,54 +2,50 @@ from unittest import TestCase
 from eventhandler import EventHandler
 
 class TestEventHandler(TestCase):
-    def setUp(self):
-        self.emgr = EventHandler('on_event1', 'on_event2', 'on_event3')
-
-    def test_bad_initialization(self):
-        with self.assertRaises(EventHandler.Exceptions.InitializationError) as context:
-            manager = EventHandler()
-
-    def test_set_allowed_callbacks(self):
-        self.assertEqual(self.emgr.allowed_callbacks, tuple(['on_event1', 'on_event2', 'on_event3']))
 
     def test_is_callable(self):
+        evntmgr = EventHandler('on_event1', 'on_event2', 'on_event3')
         not_callable_var = 'Not callable'
         callable_var = lambda : print("I'm callable")
-        self.assertFalse(self.emgr.is_callable(not_callable_var))
-        self.assertTrue(self.emgr.is_callable(callable_var))
+        self.assertFalse(evntmgr.is_callable(not_callable_var))
+        self.assertTrue(evntmgr.is_callable(callable_var))
 
     def test_bind_callback(self):
+        evntmgr = EventHandler('on_event1', 'on_event2', 'on_event3')
         callable = lambda : print(None)
 
         # Bind allowed callback
-        self.assertTrue(self.emgr.bind('on_event1', callable))
-        self.assertEqual(self.emgr.Callbacks.ON_EVENT1[0], callable)
+        self.assertTrue(evntmgr.bind('on_event1', callable))
+        self.assertEqual(evntmgr.events['on_event1'][0], callable)
 
         # Bind unallowed callback
         with self.assertRaises(EventHandler.Exceptions.EventNotAllowedError) as context:
-            self.emgr.bind('on_not_extist_event', callable)
+            evntmgr.bind('on_not_extist_event', callable)
 
         # Bind existing callback
-        self.assertFalse(self.emgr.bind('on_event1', callable))
+        self.assertFalse(evntmgr.bind('on_event1', callable))
 
         # Try to bind a not callable variable
-        self.assertFalse(self.emgr.bind('on_event2', 'impossible text var'))
+        self.assertFalse(evntmgr.bind('on_event2', 'impossible text var'))
 
     def test_unbind_callback(self):
+        evntmgr = EventHandler('on_event1', 'on_event2', 'on_event3')
         callable = lambda : print(None)
-        self.assertTrue(self.emgr.bind('on_event1', callable))
-        self.assertEqual(len(self.emgr.Callbacks.ON_EVENT1), 1)
-        self.assertTrue(self.emgr.unbind('on_event1', callable))
-        self.assertEqual(len(self.emgr.Callbacks.ON_EVENT1), 0)
-        self.assertFalse(self.emgr.unbind('on_event1', lambda: print('another function not in callback list')))
+        self.assertTrue(evntmgr.bind('on_event1', callable))
+        self.assertEqual(len(evntmgr.events['on_event1']), 1)
+        self.assertTrue(evntmgr.unbind('on_event1', callable))
+        self.assertEqual(len(evntmgr.events['on_event1']), 0)
+        self.assertFalse(evntmgr.unbind('on_event1', lambda: print('another function not in callback list')))
 
     def test_fire(self):
-        def on_fired_callback(*args, **kargs):
+        evntmgr = EventHandler('on_event1', 'on_event2', 'on_event3')
+        def on_fired_callback(*args, **kwargs):
             self.assertEqual(args[0], 1)
             self.assertEqual(args[1], 2)
             self.assertEqual(args[2], 3)
             self.assertEqual(args[3], 4)
-            self.assertEqual(kargs['extra'], 5)
+            self.assertEqual(kwargs['extra'], 5)
 
-        self.emgr.bind('on_event1', on_fired_callback)
-        self.emgr.fire('on_event1', 1, 2, 3, 4, extra=5)
+        evntmgr.bind('on_event1', on_fired_callback)
+
+        evntmgr.fire('on_event1', 1, 2, 3, 4, extra=5)
