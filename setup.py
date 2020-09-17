0509cec3eac502c75acb78cbef72f8d4af5ef943
David Vicente <dvicente74@gmail.com>
2020-09-17 02:26:39 +0100

    Refactor

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 from eventhandler import __version__, __author__
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
     name='eventhandler',
     version=__version__,
@@ -9,7 +12,7 @@ setup(
     license='MIT License',
     author=__author__,
     author_email='dvicente74@gmail.com',
-    description='A simple but effective event handler, based in calbacks, written in pure python 3',
+    description='A simple and effective event handler class, based in callbacks for python 3',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -17,7 +20,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: 6'
-    ]
+        'Programming Language :: Python :: 3'
+    ],
+    long_description=long_description
 )
--- tools/readme_code.py
+++ tools/readme_code.py
@@ -1,66 +1,108 @@
 from eventhandler import EventHandler
 
+
 class ChatRoom:
-    """Simulates a chatroom environment with event handler implementation."""
-    messages = [] # Holds user messages
-    users = {'bot':[]} # Holds
+    """Simulates a chatroom environment with event handler implementation.
+
+    This is just a documented sample without pretensions. It is not a real class implementation.
+    """
 
     def __init__(self):
-        """Initialize de chat room."""
-        # Define posible events
-        self.event_handler = EventHandler('on-newuser', 'on-message')
-        # Bind callback function to events
-        self.event_handler.bind('on-newuser', self.on_newuser_join)
-        self.event_handler.bind('on-message', self.on_message)
+        """Initialize the chat room."""
+        self.__messages = []  # Stores users messages
+        self.__users = {'bot': []}  # Stores a dictionary with registered usernames
 
-    def user_list(self):
-        """Return a list of users."""
-        return [user for user in self.users.keys() if user != 'bot']
+        # Define the event handler and make it public outside the class to let externals subscriptions to events.
+        self.event_handler = EventHandler('onNewuser', 'onMessage')  # Note that events names are cased sensitive.
 
-    def on_newuser_join(self, user):
-        """Print a message when a user join the chat room."""
-        print(f'{user} has joined the chat.')
+        # Lets link some internal class methods to those events as callbacks.
+        # You can set any number of unique events and any number of unique callbacks to fire per event,
+        # Limits are available resources.
+        self.event_handler.register_event('onNewUser')
+        self.event_handler.register_event('onMessage')
+        self.event_handler.link(self.__on_newuser_join, 'onNewuser')
+        self.event_handler.link(self.__on_message, 'onMessage')
 
-    def on_message(self, user, msg):
-        """Print the user message"""
+    # Now lets define this two methods to dispatch the events
+    # Note this methods are not accesible outside class instance
+    # This calbback will be called when onNewUser event happens
+    def __on_newuser_join(self, user):
+        """Shout in the output telling new user has joined chat room, when onNewuser event happens."""
+        print(f'** ChatRoom info ** user {user} has joined the chat ** {len(self.user_list())} user/s **')
+
+    # This callback will be called when onMessage event happens
+    def __on_message(self, user, msg):
+        """Print the user message in the output, when onMessage event happens."""
         print(f'{user} says:\t {msg}')
 
-    def say(self, user, msg):
-        """Send a messega to the chat room."""
-        if not user in self.users:
-            self.users[user] = []
-            self.event_handler.fire('on-newuser', user)
+    # Now let's define the public methods of the chatroom to be used outside the class
+    def user_list(self):
+        """Return a list of not bot users."""
+        return [user for user in self.__users.keys() if user != 'bot']
 
+    def say(self, user, msg=None):
+        """Let user (and bots) send a message to the chat room."""
+        if not user in self.__users:
+            # if user is not registered fire onNewuser event and recibe it inside the class.
+            self.__users[user] = []
+            self.event_handler.fire('onNewuser', user)
+        if not msg:
+            return
         if msg != '':
-            self.messages.append((user, msg))
-            self.event_handler.fire('on-message', user, msg)
+            # Enqueue the message and fire onMessage event to be received internally by __on_message method.
+            self.__messages.append((user, msg))
+            self.event_handler.fire('onMessage', user, msg)
 
-# Create the chatroom
-chat = ChatRoom()
 
-# This is a external callback to bind in the chatroom event habdler
-def saludate_new_user(user):
-    """Bot saludates the user."""
-    chat.say('bot', f'Hello {user}, welcome to the chat room.')
+class ChatBot:
+    """A basic bot chat that's can operate in a chatroom."""
 
-# This is a external callback to bind in the chatroom event habdler
-def response_to_user(user, msg):
-    """Bot responses to user."""
-    if user=='bot':
+    def __init__(self, chatroom: ChatRoom, name: str = 'bot'):
+        self.chatroom = chatroom
+        self.name = name
+
+        # Subscribe to external ChatRoom class events
+        chatroom.event_handler.link(self.saludate_new_user, 'onNewuser')
+        chatroom.event_handler.link(self.read_user_message, 'onMessage')
+
+    # When chatroom fires the onNewUser event our bot will saludate.
+    def saludate_new_user(self, user):
+        """Bot saludates the user."""
+        chat.say('bot', f'Hello {user}, welcome to the chat room.')
+
+    # When chatroom fires the onNewMessage event process it and broadcast some output if needed.
+    def read_user_message(self, user, msg):
+        """Read user messages and act in consequece."""
+        if user == 'bot':
+            return
+
+        # Intercept an answerable question
+        if msg == f'Hey {self.name}, are there anyone here?':
+            if len(self.chatroom.user_list()) < 1:
+                self.chatroom.say(self.name, f'Nope {user}. Just you and me.')
+            elif len(self.chatroom.user_list()) == 2:
+                self.chatroom.say(self.name, f'Yes {user}. '
+                f'there are {len(self.chatroom.user_list()) - 1} non bots users in the room, you, and me.')
+            else:
+                self.chatroom.say(self.name, f'Yes {user}. '
+                f'there are {len(self.chatroom.user_list()) - 2} non bots users in the room, you, and me.')
         return
-    if msg == 'Hey bot, are there anyone here?':
-        if len(chat.users.keys()) == 2:
-            chat.say('bot', f'Nope {user}. Just you and me.')
-        else:
-            chat.say('bot', f'Yes {user}. there are {len(chat.users.keys())-1} users in the room.')
 
-# Bind the external callbacks
-chat.event_handler.bind('on-newuser', saludate_new_user)
-chat.event_handler.bind('on-message', response_to_user)
 
-# Chat simulation
-chat.say('sergio', 'Hello World!')
-chat.say('sergio', 'Hey bot, are there anyone here?')
-chat.say('david', 'Hello everybody!')
-chat.say('david', 'Hey bot, are there anyone here?')
-chat.say('sergio', 'Hi david!')
+# Python program starts execution here
+if __name__ == '__main__':
+    # Create the chatroom
+    chat = ChatRoom()
+
+    # Now bot can control users and messages of the chat
+    bot = ChatBot(chat)
+
+    # Now the chat simulation. The first user interaction will send a message onNewuser event will be fired and
+    # managed by the bot. All messages (onMessage event) will be reached by the bot.
+    chat.say('sergio', 'Hello World!')
+    chat.say('sergio', 'Hey bot, are there anyone here?')
+    chat.say('david', 'Hello everybody!')
+    chat.say('david', 'Hey bot, are there anyone here?')
+    chat.say('sergio', 'Hi david!')
+    chat.say('kate')
+    chat.say('kate', 'Hey bot, are there anyone here?')
